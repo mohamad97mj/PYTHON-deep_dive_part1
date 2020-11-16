064f454ae331e12c7667ff1b513286bcd4f4676b
mohamad <mohamad97mj@gmail.com>
2020-11-16 17:17:42 +0330

    named tupples

---
+++ 8.4 Named Tupples.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+
+
+point2d = namedtuple('Point2d', 'x y')
+# point2d = namedtuple('Point2d', 'x _y')
+p = point2d(10, 20)
+print(p[0])
+print(p.x)
+print(p.y)
+
+for e in p:
+    print(e)
+
+p2 = point2d(x=5, y=6)
+
+# class Point2d(tuple):
+# p2.x = 100
+
+print(p2._fields)
+print(point2d._fields)
+# print(point2d._source)
+print(p2._asdict())
